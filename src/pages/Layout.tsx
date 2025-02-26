90756da8fbcc58619791cd0aabe449557229bac6
Ignasiya <ignasiya93@gmail.com>
2025-02-26 20:54:39 +0300

    classNames

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
-import clsx from "clsx";
 import { useTheme } from "../app/provider/theme/useTheme";
+import { classNames } from "../shared/utils/classNames/classNames";
 
 export default function Layout() {
   const { toggleTheme, theme } = useTheme();
 
   return (
-    <div className={clsx("app", theme)}>
+    <div className={classNames("app", {}, [theme])}>
       <nav>
         <Link to={"/"}>Главная</Link>
         <Link to={"/about"}>О нас</Link>
